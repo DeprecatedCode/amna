83336ff4fa7f613a18cbd0b6e22900f694fe2cb8
Nate Ferrero <nateferrero@gmail.com>
2014-08-17 22:29:20 -0700

    Log + cache

--- index.js
+++ index.js
@@ -25,12 +25,60 @@ var fs = require('fs');
 /**
  * Simple logging
  */
+var logCapture = [];
 var log = function () {
     var args = Array.prototype.slice.apply(arguments).map(function (x) {
         return x && x.$repr ? x.$repr : x;
     });
-    args.unshift('    AMNA ' + (new Date).toString() + '\n==>');
+    if (logCapture.length) {
+        args.unshift('...');
+        return logCapture[logCapture.length - 1].push(args);
+    }
+    if (args[0] === '...') {
+        args.unshift('  >');
+    }
+
+    else {
+        args.unshift('    AMNA ' + (new Date).toString() + '\n==>');
+    }
     console.info.apply(this, args);
+};
+
+/**
+ * Capture related logs
+ */
+log.capture = function () {
+    logCapture.push([]);
+};
+
+/**
+ * Flush related logs
+ */
+log.flush = function () {
+    var logs = logCapture.pop();
+    log.apply(this, arguments);
+    logs.map(function (lg) {
+        log.apply(this, lg)
+    });
+};
+
+/**
+ * Log in module
+ */
+log.module = function (name) {
+    var mod = function (fn) {
+        return function () {
+            var args = Array.prototype.slice.apply(arguments);
+            args.unshift('[' + name + ']');
+            return fn.apply(this, args);
+        };
+    };
+    var _log = mod(log);
+    _log.capture = log.capture;
+    _log.flush = mod(log.flush);
+    _log.error = mod(log.error);
+    _log.request = mod(log.request);
+    return _log;
 };
 
 /**
@@ -80,12 +128,14 @@ log.error = function () {
  */
 log.request = function () {
     var args = Array.prototype.slice.apply(arguments);
+    var name = args.shift();
     var req = args.shift();
     if (!req.$logId) {
         req.$logId = ((Math.random() + Math.random())/2).toString(36).replace('0.', '');
     }
     var info = '[' + [req.$logId, req.method, req.url].join(' ') + ']';
     args.unshift(info);
+    args.unshift(name);
     log.apply(this, args);
     args.forEach(function (arg) {
         if (arg instanceof Error) {
@@ -99,11 +149,17 @@ log.request = function () {
 };
 
 /**
+ * Allow modules to control loading phase
+ */
+var phases = [];
+
+/**
  * AMNA Library
  */
 [
     '$express',
     'authentication',
+    'cache',
     'collection',
     'controller',
     'err',
@@ -125,5 +181,27 @@ log.request = function () {
     'things',
     'types'
 ].map(function (name) {
-    amna[name] = require('./lib/' + name)(amna, log);
+    var mod = require('./lib/' + name);
+    if (typeof mod !== 'function') {
+        throw new Error('AMNA lib must be a function: ' + name);
+    }
+    var phase = mod.name.match(/^phase_(\d)+$/)
+    phase = phase ? phase.pop() : 0;
+    phases[phase] || (phases[phase] = []);
+    phases[phase].push({name: name, fn: mod});
+});
+
+/**
+ * Load phase-marked modules
+ */
+phases.forEach(function (mods, phase) {
+    if (!Array.isArray(mods)) {
+        return;
+    }
+    log.capture();
+    mods && mods.forEach && mods.forEach(function (mod) {
+        amna[mod.name] = mod.fn(amna, log.module(mod.name));
+        log(mod.name);
+    });
+    log.flush('Phase ' + phase + ' libraries loaded');
 });
