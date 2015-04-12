77197e889d3c38e582040f187bed0c1f9d501fe6
Nate Ferrero <nateferrero@gmail.com>
2015-04-12 05:08:04 -0700

    Fix code styles in render

--- src/lib/render.js
+++ src/lib/render.js
@@ -12,10 +12,10 @@ var fs = require('fs');
 var ejs = require('ejs');
 var path = require('path');
 
-module.exports = function (amna, log) {
+module.exports = function () {
     return function render(filename, scope, callback) {
-        fs.readFile(path.join(process.cwd(), filename), 'utf-8', function(err, data) {
-            if(err) {
+        fs.readFile(path.join(process.cwd(), filename), 'utf-8', function (err, data) {
+            if (err) {
                 callback(err);
             }
             else {
